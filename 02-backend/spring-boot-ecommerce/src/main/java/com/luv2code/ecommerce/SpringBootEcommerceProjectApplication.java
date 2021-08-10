cef96aef60922dbf653129e50ef0cf27cfde3aba
Milosz Bukala <mil.bukala@gmail.com>
2021-08-10 10:30:47 +0200

    Create repositories and changes server-port to build and open the app

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceProjectApplication.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootEcommerceProjectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootEcommerceProjectApplication.class, args);
 	}
 
