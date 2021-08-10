e47084308ea76d4b8268c186c2263804af50e43e
Milosz Bukala <mil.bukala@gmail.com>
2021-08-10 18:49:41 +0200

    modify spring boot app - rest repository by adding needed new query-method

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,18 @@
 package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // queryMethod matched by category id using 'id' parameter
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+    // sth like: SELECT * FROM product where category_id=?
+    // Spring Data REST automacically exposes endpoind
+    // http://localost:8070/api/products/search/findByCategoryId?id=2
 }
