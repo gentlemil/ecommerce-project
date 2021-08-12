ca688bbed7ad1ff678abf671a6fa9d378f883232
Milosz Bukala <mil.bukala@gmail.com>
2021-08-12 11:42:38 +0200

    create new components connected to product-category and create inside my service new observable method which maps the JSON data from the server to the product category array

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +11,37 @@ import { Product } from '../common/product';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8070/api/products';
-  // private baseUrl = 'http://localhost:8070/api/products?size=100';
-  // by default spring shows only 20 first items, by adding ?size=100 we will see 100 of them
+  // by default spring shows only 20 first items, by adding `?size=100` at the end we will see 100 of them
+  private categoryUrl = 'http://localhost:8070/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
-
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  // I'm calling REST API and returning Observable which is mapping the JSON data from Spring Data Rest to ProductCategory array*
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductsCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+
 }
 
-interface GetResponse {
+interface GetResponseProducts {     // switch from GetResponse for more qunique name of the interface
   _embedded: {
     products: Product[];
+  }
+}
+
+
+// *unwraps the JSON from Spring Data REST _embedded entry
+interface GetResponseProductsCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
